ad1b19c673b2ae20fb37391411c289abf6b0a954
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2015-11-13 14:50:47 +0100

    added pageable mapper

---
+++ src/main/java/io/beanmapper/spring/PageableMapper.java
@@ -0,0 +1,45 @@
+/*
+ * (C) 2014 42 bv (www.42.nl). All rights reserved.
+ */
+package io.beanmapper.spring;
+
+import io.beanmapper.BeanMapper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * Mapping utilities for pages.
+ *
+ * @author Jeroen van Schagen
+ * @since Nov 13, 2015
+ */
+public class PageableMapper {
+    
+    /**
+     * Converts a page into the desired target type.
+     * 
+     * @param source the source page
+     * @param targetClass the target type
+     * @param beanMapper the bean mapper used to perform mappings
+     * @return the same page, but with result type
+     */
+    public static <S, T> Page<T> map(Page<S> source, Class<T> targetClass, BeanMapper beanMapper) {
+        List<T> transformed;
+        if (source.hasContent()) {
+            List<S> content = new ArrayList<S>(source.getContent());
+            transformed = (List<T>) beanMapper.map(content, targetClass, ArrayList.class);
+        } else {
+            transformed = Collections.emptyList();
+        }
+        Pageable pageable = new PageRequest(source.getNumber(), source.getSize(), source.getSort());
+        return new PageImpl<T>(transformed, pageable, source.getTotalElements());
+    }
+
+}
---
+++ src/test/java/io/beanmapper/spring/PageableMapperTest.java
@@ -0,0 +1,62 @@
+/*
+ * (C) 2014 42 bv (www.42.nl). All rights reserved.
+ */
+package io.beanmapper.spring;
+
+import io.beanmapper.BeanMapper;
+import io.beanmapper.spring.model.Person;
+import io.beanmapper.spring.model.PersonForm;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public class PageableMapperTest {
+    
+    private final Pageable pageable = new PageRequest(0, 10, new Sort("id"));
+
+    private BeanMapper beanMapper;
+    
+    {
+        beanMapper = new BeanMapper();
+        beanMapper.addPackagePrefix(ApplicationConfig.class);
+    }
+
+    @Test
+    public void testMap() {
+        PersonForm form = new PersonForm();
+        form.name = "Henk";
+        
+        Page<PersonForm> source = new PageImpl<PersonForm>(Arrays.asList(form), pageable, 1);
+        Page<Person> target = PageableMapper.map(source, Person.class, beanMapper);
+        
+        Assert.assertNotNull(target);
+        Assert.assertEquals(pageable.getPageNumber(), target.getNumber());
+        Assert.assertEquals(pageable.getPageSize(), target.getSize());
+        Assert.assertEquals(pageable.getSort(), target.getSort());
+        Assert.assertEquals(1, target.getTotalElements());
+        
+        Person person = target.getContent().get(0);
+        Assert.assertEquals(Person.class, person.getClass());
+        Assert.assertEquals("Henk", person.getName());
+    }
+    
+    @Test
+    public void testMapEmpty() {
+        Page<PersonForm> source = new PageImpl<PersonForm>(Arrays.<PersonForm> asList(), pageable, 0);
+        Page<Person> target = PageableMapper.map(source, Person.class, beanMapper);
+
+        Assert.assertNotNull(target);
+        Assert.assertEquals(pageable.getPageNumber(), target.getNumber());
+        Assert.assertEquals(pageable.getPageSize(), target.getSize());
+        Assert.assertEquals(pageable.getSort(), target.getSort());
+        Assert.assertEquals(0, target.getTotalElements());
+    }
+
+}
