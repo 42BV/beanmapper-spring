a7156a5aa53bb2d7262ffa5353be4beb9a7a5795
marcus-talbot42 <marcus.talbot@42.nl>
2022-09-16 10:11:36 +0200

    Proposed fix for #138 in BeanMapper.
    - Fixed an issue where the HibernateAwareBeanUnproxy
      would attempt to return an interface of a static
      nested class, due to it having a $-character in its
      name. This was fixed by checking whether the given
      class implements HibernateProxy. If not, we simply
      return the given class back again.
    - Updated Changelog

--- src/main/java/io/beanmapper/spring/unproxy/HibernateAwareBeanUnproxy.java
+++ src/main/java/io/beanmapper/spring/unproxy/HibernateAwareBeanUnproxy.java
@@ -1,5 +1,7 @@
 package io.beanmapper.spring.unproxy;
 
+import java.util.Arrays;
+
 import io.beanmapper.core.unproxy.BeanUnproxy;
 
 import org.hibernate.proxy.HibernateProxy;
@@ -9,16 +11,10 @@ public class HibernateAwareBeanUnproxy implements BeanUnproxy {
     /**
      * {@inheritDoc}
      */
-    @Override
     public Class<?> unproxy(Class<?> beanClass) {
-        String name = beanClass.getName();
-        if (name.contains("$")) {
-            Class<?> interfaceClass = beanClass.getInterfaces()[0];
-            if (interfaceClass.equals(HibernateProxy.class)) {
-                return beanClass.getSuperclass();
-            } else {
-                return beanClass.getInterfaces()[0];
-            }
+        if (beanClass.getName().contains("$")
+                && Arrays.asList(beanClass.getInterfaces()).contains(HibernateProxy.class)) {
+            return beanClass.getSuperclass();
         }
         return beanClass;
     }
--- src/test/java/io/beanmapper/spring/unproxy/HibernateAwareBeanUnproxyTest.java
+++ src/test/java/io/beanmapper/spring/unproxy/HibernateAwareBeanUnproxyTest.java
@@ -2,17 +2,41 @@ package io.beanmapper.spring.unproxy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.beanmapper.spring.model.Spoon;
+import io.beanmapper.spring.model.SpoonDrawerForm;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class HibernateAwareBeanUnproxyTest {
+class HibernateAwareBeanUnproxyTest {
+
+    private HibernateAwareBeanUnproxy beanUnproxy;
+
+    @BeforeEach
+    void setUp() {
+        this.beanUnproxy = new HibernateAwareBeanUnproxy();
+    }
 
     @Test
-    public void unproxyHibernateProxy() {
+    void unproxyHibernateProxy() {
         HibernateProxyContainer shopHibernateProxy = new HibernateProxyContainer();
         Object object = shopHibernateProxy.new GeneratedProxy();
-        HibernateAwareBeanUnproxy beanUnproxy = new HibernateAwareBeanUnproxy();
-        Class unproxyClass = beanUnproxy.unproxy(object.getClass());
+        Class<?> unproxyClass = beanUnproxy.unproxy(object.getClass());
         assertEquals(Shop.class, unproxyClass);
+    }
+
+    @Test
+    void testUnproxy_ShouldNotConsiderStaticNestedClassAHibernateProxy() {
+        Object object = new SpoonDrawerForm.SpoonForm();
+        Class<?> unproxyClass = beanUnproxy.unproxy(object.getClass());
+        assertEquals(SpoonDrawerForm.SpoonForm.class, unproxyClass);
+    }
+
+    @Test
+    void testUnproxy_ShouldReturnObjectUnchangedWhenNotAHibernateProxy() {
+        Object object = new Spoon(true);
+        Class<?> unproxyClass = beanUnproxy.unproxy(object.getClass());
+        assertEquals(Spoon.class, unproxyClass);
     }
 
 }
